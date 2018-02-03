a9b17adf883509cdb1dc00e06cec6dad7e1e13e8
kevin12686 <kevin860612@gmail.com>
2018-02-03 22:24:18 +0800

    record_detail & record_update

--- Billing/urls.py
+++ Billing/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.record_list.as_view(), name='record_list'),
+    path('detail/<pk>', views.record_detail.as_view(), name='record_detail'),
+    path('update/<pk>', views.record_update.as_view(), name='record_update'),
 ]
--- Billing/views.py
+++ Billing/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, DeleteView
+from django.urls import reverse
+from django.contrib import messages
+from django.views.generic import ListView, DetailView, DeleteView, UpdateView
 
 from . import models
 
@@ -8,4 +10,20 @@ from . import models
 
 class record_list(ListView):
     model = models.Record
+    ordering = 'transaction_time'
     template_name = 'record_list.html'
+
+
+class record_detail(DetailView):
+    model = models.Record
+    template_name = 'record_detail.html'
+
+
+class record_update(UpdateView):
+    model = models.Record
+    fields = ['transaction_time', 'record_type', 'amount', 'fee']
+    template_name = 'record_form.html'
+
+    def get_success_url(self):
+        messages.success(self.request, '更新成功')
+        return reverse('record_list')
